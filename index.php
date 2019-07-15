9bc7a6001ba0b24a099012458bf1c6b114b5f057
Unknown <ntaylor0377@gmail.com>
2019-07-15 23:04:39 +1000

    getting started

---
+++ index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!-- GoogleFont -->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap" rel="stylesheet">
+  <!-- FontAwesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
+  <!-- AnimateCSS -->
+  <link rel="stylesheet" href="assets/animate.css">
+  <!-- CSS File -->
+  <link rel="stylesheet" href="assets/20190714_style.css">
+
+  <title>ntaylor.tech</title>
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="nav-container">
+      <div class="float-left nt-box text-center">
+        NT
+      </div>
+      <div class="float-right nav-links">
+        <ul class="list-inline">
+          <li class="list-inline-item"><a href="#">Home</a></li>
+          <li class="list-inline-item"><a href="#">Projects</a></li>
+          <li class="list-inline-item"><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="container text-center mt-5">
+    <h1>Hi, I'm Nathan ;)</h1>
+
+    <p class="mt-3 mb-3 hero" style="letter-spacing: 1px;">
+      programmer | tinkerer | linux_enthusiast | tech_geek
+    </p>
+
+    <div class="hero-links">
+      <ul class="list-inline">
+        <li class="list-inline-item"><a href="https://github.com/ntaylor-86" target="_blank"><i
+              class="fab fa-github mr-1"></i></a></li>
+        <li class="list-inline-item"><a href="https://twitter.com/N_Taylr" target="_blank"><i
+              class="fab fa-twitter ml-1"></i></a></li>
+      </ul>
+    </div>
+
+    <div class="hello-world-wrapper">
+      <img src="assets/echo_hello_world.jpg" class="img-fluid hello-world-image" alt="hello world">
+    </div>
+
+  </div>
+
+
+  <div class="container mt-5">
+    <h3>_ Projects</h3>
+    <p>
+
+    </p>
+  </div>
+
+  <div class="container mt-5">
+    <h3>_ Contact</h3>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
